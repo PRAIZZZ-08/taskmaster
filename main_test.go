40d9a0357265912dac6d8e1c173eec6d6d4fbce6
PRAIZZZ-08 <praizzzdev@gmail.com>
2026-05-05 17:45:14 +0100

    test: updated test logic
    hello

--- main_test.go
+++ main_test.go
@@ -4,11 +4,19 @@ import "testing" // [testing]: The built-in package for unit tests [4]
 
 // [TestCheck]: Must start with 'Test' and take *testing.T
 func TestCheck(t *testing.T) {
+<<<<<<< HEAD
 	task := Task{Title: "Test Task"}
 	expected := "Audited: Test Task with cost 0"
 
 	// [Check]: Running the actual task logic
 	result := task.Check()
+=======
+	project := Project{Title: "Test Project"}
+	expected := "Audited: Test Project with cost 0"
+
+	// [Check]: Running the actual project logic
+	result := project.Check()
+>>>>>>> 8ccf087 (test: updated test logic)
 
 	if result != expected {
 		// [Errorf]: Reports a failure and what the expected vs actual was
